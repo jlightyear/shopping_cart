685cd1706c373bacd6e791f9cdde2f82c27ce25a
jlightyear <joseluis.estrach@gmail.com>
2015-04-07 23:57:54 +0200

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create! id: 1, name: "Banana", price: 0.49, active: true
+Product.create! id: 2, name: "Apple", price: 0.29, active: true
+Product.create! id: 3, name: "Box of Strawberries", price: 1.99, active: true
+Product.create! id: 4, name: "Orange", price: 0.25, active: true
+Product.create! id: 5, name: "Pear", price: 0.28, active: true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
