c4d4596c3742cc71cdacc5f3c8270302d425dffc
jlightyear <joseluis.estrach@gmail.com>
2015-04-08 16:51:39 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'order_items/create'
-
-  get 'order_items/update'
-
-  get 'order_items/destroy'
-
-  get 'carts/show'
-
-  get 'products/index'
+  
+  root to: "products#index"
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
